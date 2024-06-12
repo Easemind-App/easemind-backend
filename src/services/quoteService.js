461f5057ee4f4273bbadd5e60ed6d95b5a697a41
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-06-12 15:28:13 +0700

    Added Quote Service

---
+++ src/services/quoteService.js
@@ -0,0 +1,27 @@
+const db = require('../utils/db')
+
+const getQuoteBySentiment = async (sentiment) => {
+  try {
+    const predefinedLimit = 40
+
+    const randomIndex = Math.floor(Math.random() * predefinedLimit)
+
+    const quoteDoc = await db
+      .collection('quotes')
+      .doc(sentiment)
+      .collection('quotes')
+      .doc(randomIndex.toString())
+      .get()
+
+    if (!quoteDoc.exists) {
+      return null
+    }
+
+    return quoteDoc.data().text
+  } catch (error) {
+    console.error('Error fetching quote by sentiment:', error)
+    throw new Error('Error fetching quote by sentiment.')
+  }
+}
+
+module.exports = { getQuoteBySentiment }
